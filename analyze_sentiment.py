5586b028f6a527194cb22648099018949c1375f3
Freddyharari <124592567+Freddyharari@users.noreply.github.com>
2024-06-17 14:30:46 -0300

    Update analyze_sentiment.py

--- analyze_sentiment.py
+++ analyze_sentiment.py
@@ -1,13 +1,1 @@
-from transformers import pipeline
 
-# Criar uma pipeline de análise de sentimentos
-sentiment_analysis = pipeline("sentiment-analysis")
-
-# Texto a ser analisado
-text = "I am very happy with the service!"
-
-# Analisar o sentimento do texto
-result = sentiment_analysis(text)
-
-# Imprimir o resultado
-print(result)
